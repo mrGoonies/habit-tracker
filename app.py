5a2b394572ab6f218f0a32e70796b8c5aedecadb
mrGoonies <gooniesdev@gmail.com>
2023-10-17 21:39:03 -0300

    Agregando estilos al proyecto

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+list_of_habits = list()
+
 
 @app.get("/")
 @app.get("/home")
 def index():
-    return render_template("index.html", title="Home")
+    return render_template("index.html", title="Home", list_of_habits=list_of_habits)
+
 
 @app.get("/add")
 @app.post("/add")
 def add_habit():
+    if request.method == "POST":
+        list_of_habits.append(request.form.get("habit"))
+        print(list_of_habits)
 
-    return render_template("add.html", title="Add Habit")
+    return render_template("add_habit.html", title="Add Habit")
